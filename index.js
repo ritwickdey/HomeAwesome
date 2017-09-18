a4e404cef9736610515de994a2d6e6effa4c99f3
Ritwick Dey <ritwickdey@outlook.com>
2017-09-18 19:40:42 +0530

    time added

--- index.js
+++ index.js
@@ -14,13 +14,13 @@
             return 'Good Morning.';
         else if (hrs >= 12 && hrs <= 17)
             return 'Good Afternoon.';
-        else (hrs >= 17 && hrs <= 24)
-            return 'Good Evening.';
+        else(hrs >= 17 && hrs <= 24)
+        return 'Good Evening.';
 
     }
 
     let options = {
-        strings: ["","Hi there...", getGettingMsg(), "It's really awesome. ^200 Isn't it? ", "Fork me on <a href='https://github.com/ritwickdey/HomeAwesome'>GitHub<a> to add features ^200"],
+        strings: ["", "Hi there...", getGettingMsg(), "It's really awesome. ^200 Isn't it? ", "Fork me on <a href='https://github.com/ritwickdey/HomeAwesome'>GitHub<a> to add features ^200"],
         typeSpeed: 40,
         smartBackspace: true,
 
@@ -33,5 +33,13 @@
     }
 
     let typed = new Typed("#welcomeMsg", options);
+
+    let updateTimeBySec = setInterval(() => {
+            let timeElem = document.getElementById('time');
+            timeElem.innerText = moment().format("hh:mm:ss a");;
+    }, 1000);
+
+
+    updateTimeBySec();
 
 })();
