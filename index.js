6db8bb44629aa7eade2b0e0953052b28dedb117a
Ritwick Dey <ritwickdey@outlook.com>
2017-09-18 13:32:40 +0530

    typed.js & getting msg added

---
+++ index.js
@@ -0,0 +1,36 @@
+;
+(function () {
+
+    if (!Typed) {
+        console.error("Typed.Js is not found.");
+        return;
+    }
+
+    getGettingMsg = () => {
+        let hrs = (new Date()).getHours();
+
+        if (hrs < 12)
+            return 'Good Morning.';
+        else if (hrs >= 12 && hrs <= 17)
+            return 'Good Afternoon.';
+        else (hrs >= 17 && hrs <= 24)
+            return 'Good Evening.';
+
+    }
+
+    let options = {
+        strings: ["Hi there...", getGettingMsg(), "It's really awesome. ^200 Isn't it? ", "Fork me on GitHub to add features"],
+        typeSpeed: 40,
+        smartBackspace: true,
+
+        backSpeed: 20,
+        shuffle: !true,
+        loop: true,
+        loopCount: Infinity,
+        showCursor: !false,
+        cursorChar: '|'
+    }
+
+    let typed = new Typed("#welcomeMsg", options);
+
+})();
